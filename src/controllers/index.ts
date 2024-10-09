9ef3a7492fd804d149a8b58a66e7121fc4a78341
Daustinn <daustinn.music@gmail.com>
2024-10-09 18:34:29 -0500

    refactor: Update getPerson function to store person details in Redis cache
    This change refactors the `getPerson` function in the `src/controllers/index.ts` file to store the retrieved person details in the Redis cache. The function now checks if the person details are already present in the cache before querying the database. If the details are not found in the cache, the function retrieves them from the database and stores them in the cache for future use.
    
    This improvement enhances the performance of the application by reducing the number of database queries for the same person and utilizing the Redis cache for faster data retrieval.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -20,26 +20,57 @@ export const getPerson = async (
     const ilp = await getConnectionIlp();
     const elp = await getConnectionElp();
 
-    let person = null;
+    let match = null;
 
-    const query = `SELECT people.codigo, people.paterno, people.materno, people.nombres, carer.nomesp FROM alumno AS people
-       JOIN tb_ficha_perso_alu as tfpa on people.codigo =  tfpa.c_codalu
-       JOIN tb_especialidad as carer on people.c_codesp = carer.codesp
-       WHERE people.codigo = ?`;
+    const query = `SELECT
+                  a.codigo,
+                  a.paterno,
+                  a.materno,
+                  a.nombres,
+                  te.c_abrevesp,
+                  tfpa.c_sexo,
+                  tfpa.c_email_institucional,
+                  tmf.n_codper,
+                  tp.c_perlit
+                FROM
+                  alumno a
+                  JOIN tb_ficha_perso_alu tfpa ON a.codigo = tfpa.c_codalu
+                  JOIN tb_especialidad te ON a.c_codesp = te.codesp
+                  JOIN tb_mat_fic tmf ON a.codigo = tmf.c_codalu
+                  AND a.facultad = tmf.c_codfac
+                  AND a.c_codesp = tmf.c_codesp
+                  JOIN tb_peracd tp ON tp.n_codper = tmf.n_codper
+                WHERE
+                  a.codigo = ?
+                ORDER BY
+                  tmf.n_codper DESC
+                LIMIT
+                  1`;
 
     const [rowsElp] = (await elp.query(query, [req.params.id])) as any;
 
     if (rowsElp[0]) {
-      person = rowsElp[0];
+      match = rowsElp[0];
     } else {
       const [rowsIlp] = (await ilp.query(query, [req.params.id])) as any;
       if (rowsIlp[0]) {
-        person = rowsIlp[0];
+        match = rowsIlp[0];
       }
     }
 
-    if (!person) throw new Error("Person not found");
+    if (!match) throw new Error("Person not found");
 
+    const person = {
+      id: match.codigo,
+      firstName: match.nombres.trim(),
+      lastName: `${match.paterno} ${match.materno}`,
+      email: match.c_email_institucional,
+      career: match.c_abrevesp,
+      sex: match.c_sexo,
+      periodCode: match.n_codper,
+      periodName: match.c_perlit,
+      institution: rowsElp[0] ? "ELP" : "ILP",
+    };
     await redisClient.set(`person_${id}`, JSON.stringify(person), {
       EX: 60 * 60 * 24 * 30,
     });
