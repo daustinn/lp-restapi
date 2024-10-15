72313d6c2ba47a993e92076b0acfd186ab02bb4e
Daustinn <daustinn.music@gmail.com>
2024-10-15 15:16:46 -0500

    refactor: Update getPerson function to store person details in Redis cache and handle different institutions

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import redisClient from "../config/redis";
-import { getConnectionElp, getConnectionIlp } from "../config/database";
+import {
+  closeConnectionElp,
+  closeConnectionIlp,
+  connectToMysqlElp,
+  connectToMysqlIlp,
+  getConnectionElp,
+  getConnectionIlp,
+} from "../config/database";
 
 export const getPerson = async (
   req: Request,
@@ -16,71 +23,77 @@ export const getPerson = async (
 
     if (cachedPerson) {
       res.json(JSON.parse(cachedPerson));
+    } else {
+      await connectToMysqlElp();
+      await connectToMysqlIlp();
+
+      const ilp = await getConnectionIlp();
+      const elp = await getConnectionElp();
+
+      let match = null;
+
+      const query = `SELECT
+                    a.codigo,
+                    a.paterno,
+                    a.materno,
+                    a.nombres,
+                    te.nomesp,
+                    tfpa.c_sexo,
+                    tfpa.c_email_institucional,
+                    tmf.n_codper,
+                    tp.c_perlit
+                  FROM
+                    alumno a
+                    JOIN tb_ficha_perso_alu tfpa ON a.codigo = tfpa.c_codalu
+                    JOIN tb_especialidad te ON a.c_codesp = te.codesp
+                    JOIN tb_mat_fic tmf ON a.codigo = tmf.c_codalu
+                    AND a.facultad = tmf.c_codfac
+                    AND a.c_codesp = tmf.c_codesp
+                    JOIN tb_peracd tp ON tp.n_codper = tmf.n_codper
+                  WHERE
+                    a.codigo = ?
+                  ORDER BY
+                    tmf.n_codper DESC
+                  LIMIT
+                    1`;
+
+      if (institution == "ilp") {
+        const [rowsIlp] = (await ilp.query(query, [req.params.id])) as any;
+        if (rowsIlp[0]) match = rowsIlp[0];
+      }
+
+      if (institution == "elp") {
+        const [rowsElp] = (await elp.query(query, [req.params.id])) as any;
+
+        if (rowsElp[0]) match = rowsElp[0];
+      }
+
+      if (!match) throw new Error("Person not found");
+
+      const person = {
+        id: match.codigo,
+        firstName: match.nombres.trim(),
+        lastName: match.paterno,
+        lastName_2: match.materno,
+        email: match.c_email_institucional,
+        career: match.nomesp,
+        sex: match.c_sexo,
+        periodCode: match.n_codper,
+        periodName: match.c_perlit,
+        institution:
+          institution === "ilp"
+            ? "Instituto La Pontificia"
+            : "Escuela Superior La Pontificia",
+      };
+      await redisClient.set(`person_${id}`, JSON.stringify(person), {
+        EX: 60 * 60 * 24 * 30,
+      });
+
+      closeConnectionElp();
+      closeConnectionIlp();
+
+      res.json(person);
     }
-
-    const ilp = await getConnectionIlp();
-    const elp = await getConnectionElp();
-
-    let match = null;
-
-    const query = `SELECT
-                  a.codigo,
-                  a.paterno,
-                  a.materno,
-                  a.nombres,
-                  te.nomesp,
-                  tfpa.c_sexo,
-                  tfpa.c_email_institucional,
-                  tmf.n_codper,
-                  tp.c_perlit
-                FROM
-                  alumno a
-                  JOIN tb_ficha_perso_alu tfpa ON a.codigo = tfpa.c_codalu
-                  JOIN tb_especialidad te ON a.c_codesp = te.codesp
-                  JOIN tb_mat_fic tmf ON a.codigo = tmf.c_codalu
-                  AND a.facultad = tmf.c_codfac
-                  AND a.c_codesp = tmf.c_codesp
-                  JOIN tb_peracd tp ON tp.n_codper = tmf.n_codper
-                WHERE
-                  a.codigo = ?
-                ORDER BY
-                  tmf.n_codper DESC
-                LIMIT
-                  1`;
-
-    if (institution == "ilp") {
-      const [rowsIlp] = (await ilp.query(query, [req.params.id])) as any;
-      if (rowsIlp[0]) match = rowsIlp[0];
-    }
-
-    if (institution == "elp") {
-      const [rowsElp] = (await elp.query(query, [req.params.id])) as any;
-
-      if (rowsElp[0]) match = rowsElp[0];
-    }
-
-    if (!match) throw new Error("Person not found");
-
-    const person = {
-      id: match.codigo,
-      firstName: match.nombres.trim(),
-      lastName: match.paterno,
-      lastName_2: match.materno,
-      email: match.c_email_institucional,
-      career: match.nomesp,
-      sex: match.c_sexo,
-      periodCode: match.n_codper,
-      periodName: match.c_perlit,
-      institution:
-        institution === "ilp"
-          ? "Instituto La Pontificia"
-          : "Escuela Superior La Pontificia",
-    };
-    await redisClient.set(`person_${id}`, JSON.stringify(person), {
-      EX: 60 * 60 * 24 * 30,
-    });
-
-    res.json(person);
   } catch (error) {
     next(error);
   }
